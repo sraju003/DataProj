0cdcde804873688e205773c6bd720e6abc23a21a
SAMBASIVA <sraju003@odu.edu>
2014-07-31 18:45:22 -0500

    fpr loop refactored

--- Data Proj/src/com/core/basic/Demo.java
+++ Data Proj/src/com/core/basic/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		Marks marks=new Marks();
-		int[][] marksArray={{50,60,55,67,70,62},{62,65,70,70,81,74},{72,66,77,80,69,62}};
+		int[][] marksArray={{52,60,55,67,70,62},{65,68,73,68,84,77},{69,63,74,77,66,59}};
 		double[][] marksList=marks.generateMarksSheet(marksArray);
 		marks.display(marksList);
 	}
